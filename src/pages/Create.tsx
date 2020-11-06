fa2481301734f8086817711ed4364a42ecb481a7
HSSNPdS <hssnpds@gmail.com>
2020-11-06 15:32:31 -0300

    Estilização inicial

--- src/pages/Create.tsx
+++ src/pages/Create.tsx
@@ -3,10 +3,22 @@ import { Text, View, StyleSheet, Image, TouchableOpacity } from 'react-native';
 
 function Create(){
   return(
-    <View>
+    <View style={styles.container}>
       
     </View>
   )
 }
 
 export default Create;
+
+const styles = StyleSheet.create({
+  container:{
+    flex: 1,
+    
+    alignItems: 'center',
+    alignContent: 'center',
+    justifyContent: 'center',
+
+    backgroundColor:'#e6e6e6'
+  }
+})
